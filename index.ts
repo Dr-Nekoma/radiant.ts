fd308b00584bb455056fb5d70a7a33ac18ff16db
EduardoLR10 <dudulr10@gmail.com>
2023-02-28 21:41:08 -0300

    Add main function

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 // import { add, multiply } from "./deps.ts";
 
-const canvas = document.querySelector("canvas")as HTMLCanvasElement;
-const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-const image = document.querySelector("#ship") as HTMLImageElement;
-let xCoordinate = 10;
-let yCoordinate = 10;
+function main() : void {
+    const canvas = document.querySelector("canvas")as HTMLCanvasElement;
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const image = document.querySelector("#ship") as HTMLImageElement;
+    let xCoordinate = 10;
+    let yCoordinate = 10;
+    
+    image.addEventListener("load", (e) => {
+	context.drawImage(image, xCoordinate, yCoordinate, 200, 200);
+	document.addEventListener("keydown", (event) => {
+	    context.clearRect(0, 0, 500, 500);
+	    let coordinates = updateCoordinates(event.key, xCoordinate, yCoordinate);
+	    xCoordinate = coordinates[0];
+	    yCoordinate = coordinates[1];
+            context.drawImage(image, xCoordinate, yCoordinate, 200, 200);
+	});
+    });
 
-function updateCoordinates(key: string) {
+}
+
+function updateCoordinates(key: string, xCoordinate: number, yCoordinate: number) : [number, number] {
     if(key == "ArrowRight"){
 	xCoordinate += 10;
     } else if(key == "ArrowDown") {
@@ -16,14 +30,7 @@ function updateCoordinates(key: string) {
     } else if(key == "ArrowUp") {
 	yCoordinate -= 10;	
     }
+    return [xCoordinate, yCoordinate];
 }
 
-image.addEventListener("load", (e) => {
-    context.drawImage(image, xCoordinate, yCoordinate, 200, 200);
-    document.addEventListener("keydown", (event) => {
-	context.clearRect(0, 0, 500, 500);
-	updateCoordinates(event.key);
-        context.drawImage(image, xCoordinate, yCoordinate, 200, 200);
-    });
-});
-
+main();
