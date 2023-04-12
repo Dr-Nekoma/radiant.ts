e69330842d574b9ee21a82d1d799e735b250e3cb
EduardoLR10 <dudulr10@gmail.com>
2023-04-11 22:37:57 -0300

    Fix hitbox mechanism

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const PLAYER_HEIGHT = 200;
 const ENEMY_WIDTH = 70;
 const ENEMY_HEIGHT = 70;
 const PROJECTILE_WIDTH = 5;
-const PROJECTILE_HEIGHT = 10;
+const PROJECTILE_HEIGHT = 5;
 
 // type Weapon = {
 //     name: string;
@@ -28,11 +28,12 @@ type Enemy = {
     width: number; 
     height: number;       
     currentCoordinates: Coordinates;
-	// color: string;
-	// gunType: Weapon[];
+    // movement: (arg0: Enemy) => (Enemy);
+    // color: string;
+    // gunType: Weapon[];
 };
 
-// TODO: There is some accumulation of the previous positions of the projectile in the game State. We should fix that
+// TODO: There is some accumulation of the previous positions of the projectile in the game State. We should fix that.
 type Projectile = {
     width: number; 
     height: number;           
@@ -41,6 +42,11 @@ type Projectile = {
 	// format: string;
 	// direction: string;
 };
+
+function giveMe(): Projectile {
+    return { width: 100, height: 100, currentCoordinates: [100,100]};
+}
+
 
 namespace State {
 	export type Done = {
@@ -71,58 +77,57 @@ namespace Spaceship {
 
 }
 
+let throttleFire = throttle(20, fire);
 
 function fire(state: Game): Game {
 	const { player, projectiles } = state;
-    const p: Projectile = { currentCoordinates: player.currentCoordinates, width: PROJECTILE_WIDTH, height: PROJECTILE_HEIGHT };
+    const p: Projectile = { currentCoordinates: [player.currentCoordinates[0], player.currentCoordinates[1] - 50], width: PROJECTILE_WIDTH, height: PROJECTILE_HEIGHT };
 	return { ...state, projectiles: [...projectiles, p] };
 }
-
 
 function main(): void {
 	const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 	const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 	const imagePlayer = document.querySelector("#ship") as HTMLImageElement;
 	const imageEnemy = document.querySelector("#enemy") as HTMLImageElement;
-
+    giveMe();
     let ex: Spaceship = { currentCoordinates: [10, 10], width: PLAYER_WIDTH, height: PLAYER_HEIGHT };
     let en1: Enemy = { currentCoordinates: [100, 100], width: ENEMY_WIDTH, height: ENEMY_HEIGHT };
     let gameState: Game = { projectiles: [], player: ex, enemies: [en1] };
-	let input: FP.option.Option<string> = FP.option.none;
+    let input: FP.option.Option<string> = FP.option.none;
 
-	imagePlayer.addEventListener("load", (e) => {
-		context.drawImage(imagePlayer, ex.currentCoordinates[0], ex.currentCoordinates[1], 200, 200);
-		document.addEventListener("keydown", (event) => {
-			input = FP.option.some(event.key);
-		});
-		document.addEventListener("keyup", (_event) => {
-			input = FP.option.none;
-		});
+
+	window.addEventListener("load", (e) => {
+	    context.drawImage(imagePlayer, ex.currentCoordinates[0], ex.currentCoordinates[1], 200, 200);
+	    let handler: number;
+	    document.addEventListener("keydown", (event) => {
+		clearTimeout(handler);
+		input = FP.option.some(event.key);
+		handler = setTimeout(() => input = FP.option.none, 16);
+	    });
 	});
 
 	let time = 0;
 	setInterval(() => {
 		context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-		gameState = updateWithAction(input, gameState);
+	    gameState = updateWithAction(input, gameState);	    
 		gameState = updateProjectiles(gameState);
 		context.drawImage(imagePlayer, gameState.player.currentCoordinates[0], gameState.player.currentCoordinates[1], PLAYER_WIDTH, PLAYER_HEIGHT);
 		gameState = enemyMovement1(time, gameState);
-		// console.log(gameState.enemies[0].currentCoordinates);
 		context.drawImage(imageEnemy, gameState.enemies[0].currentCoordinates[0], gameState.enemies[0].currentCoordinates[1], ENEMY_WIDTH, ENEMY_HEIGHT);
 		gameState.projectiles.forEach(p => {
 			context.fillStyle = "0xFF0000";
 			context.fillRect(p.currentCoordinates[0], p.currentCoordinates[1], 5, 10);
 		});
 	    gameState = updatePresence(gameState);
-	    // console.log(gameState.projectiles);
 	    time += 0.016;	    
 	}, 16);
 
 }
 
-// TODO: Review this folding function, because it does not in fact work
+// TODO: Review this folding function, because it does not in fact work. We should use filters.
 function updatePresence(gameState: Game) : Game {
-    let nextGameState = Object.assign({}, gameState);
+    let nextGameState = structuredClone(gameState);
     nextGameState.projectiles = [];
     console.log(gameState.projectiles)
     return FP.array.reduce(nextGameState, (acc: Game, projectile: Projectile) => {
@@ -155,7 +160,7 @@ function wasHit(current: Coordinates, width: number, height: number, projectile:
     let entityBottomRightCorner: Coordinates = [current[0] + width, current[1] + height];
     
     return [pointUpLeftCorner, pointBottomRightCorner].some((point) => {
-	isPointContained(entityUpLeftCorner, entityBottomRightCorner, point);
+	return isPointContained(entityUpLeftCorner, entityBottomRightCorner, point);
     });
 }
 
@@ -167,7 +172,7 @@ function isPointContained(upLeftCorner: Coordinates, bottomRightCorner: Coordina
     let yBottomRightCorner = bottomRightCorner[1];
 
     let xPoint = point[0];
-    let yPoint = point[1];    
+    let yPoint = point[1];
 
     if (xPoint >= xUpLeftCorner && xPoint <= xBottomRightCorner && yPoint >= yUpLeftCorner && yPoint <= yBottomRightCorner){
 	return true;
@@ -211,12 +216,22 @@ function updateWithAction(optionKey: FP.option.Option<string>, g: Game): Game {
 				xCoordinate -= 15;
 			} else if (key == "ArrowUp") {
 				yCoordinate -= 15;
-			} else if (key == "z") {
+			} else if (key == "z") {			    
 				newG = fire(g);
 			}
 			return { ...newG, player: Spaceship.updateCoordinates(s, [xCoordinate, yCoordinate]) };
 		}
 	))(optionKey);
+}
+
+function throttle<A, B>(delay: number, func: (arg0: A) => B) {
+    let current = Date.now();
+    return (args: (A)) => {
+	if(Date.now() - current > delay) {
+	    func(args);
+	    current = Date.now();
+	}
+    };
 }
 
 main();
